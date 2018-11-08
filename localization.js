02ac5953c89ae7a253f277f23a5f719c1e4c5a87
rammba <40705899+rammba@users.noreply.github.com>
2018-11-08 15:00:31 +0100

    Add files via upload

---
+++ localization.js
@@ -0,0 +1,119 @@
+	"Srpski(lat)": {
+        "author": "Serbian latin by <a href='https://steamcommunity.com/id/rammbagrobar/'>rammba</a> and <a href='https://steamcommunity.com/id/Romanizat'>romanizat</a>",
+        "locale": "srb(lat)",
+        "progress_communityunlocks": "Zajednica je otključala",
+        "progress_bar_title": "Progres: %1%",
+        "progress_locked": "Zaključano",
+        "progress_beetletaur": "Vatreni Svinjaklop",
+        "progress_boarrior": "Veliki Forge Veparatnik",
+        "progress_book_elemental": "Tom prizivanja i spiralnog koplja",
+        "progress_lavaarena_armor_hpextraheavy": "Veliki istrajni oklop",
+        "progress_lavaarena_armor_hpdamager": "Veliki zupčasti oklop",
+        "progress_lavaarena_armor_hprecharger": "Veliki svileni oklop",
+        "progress_lavaarena_armor_hppetmastery": "Veliki šaputajući oklop",
+        "progress_lavaarena_heavyblade": "Kovačeva Oštrica",
+        "progress_lavaarena_firebomb": "Kristalizovana odaja magme",
+        "progress_rhinodrill": "Pesnicarog",
+        "progress_trails": "Veparila",
+        "progress_lastsync": "Poslednji put ažurirano %1",
+        "progress_lastsync_title": "Poslednji put ažurirano %1",
+        "leaderboards-title": "Najbolji timovi",
+        "leaderboards-loading": "Učitavanje još timova...",
+        "leaderboards-noresults": "Nema rezultata.. Pokušajte kasnije",
+        "leaderboards-pos": "%1",
+        "leaderboards-time": "Vreme",
+        "leaderboards-seconds": "s",
+        "leaderboards-deaths": "Broj smrti",
+        "leaderboards-showmore": "Prikaži više",
+        "leaderboards-showless": "Prikaži manje",
+        "date": {
+            "parentLocale": 'en',
+            "months": 'Januara_Februara_Marta_Aprila_Maja_Juna_Jula_Avgusta_Septembra_Oktobra_Novembra_Decembra'.split('_'),
+            "longDateFormat": {
+				LLL: 'D[.]  MMMM  YYYY[. u]  HH:mm'
+				
+            },
+            "relativeTime": {
+                "future": "za %s",
+                "past": "pre %s",
+                "s": 'par sekundi',
+                "ss": '%d sekundi',
+                "m": "jednog minuta",
+                "mm": "%d minuta",
+                "h": "jednog sata",
+                "hh": "%d sati",
+                "d": "jednog dana",
+                "dd": "%d dana",
+                "M": "jednog meseca",
+                "MM": "%d meseci",
+                "y": "jedne godine",
+                "yy": "%d godina"
+            },
+            "ordinal": function(number) {
+                return number + ".";
+            }
+        },
+        "localization-credits": "Zasluge lokalizacija",
+        "localization-help": "Da li želiš da pomogneš?",
+        "localization-helptext": "Želiš li dodati svoj jezik? Podnesi zahtev na %1."
+	},
+	
+	"Srpski(ćir)": {
+        "author": "Serbian cyrillic by <a href='https://steamcommunity.com/id/rammbagrobar/'>rammba</a>",
+        "locale": "srb(cyr)",
+        "progress_communityunlocks": "Заједница је откључала",
+        "progress_bar_title": "Прогрес: %1%",
+        "progress_locked": "Закључано",
+        "progress_beetletaur": "Ватрени Свињаклоп",
+        "progress_boarrior": "Велики Вепаратник",
+        "progress_book_elemental": "Том призивања и спиралног копља",
+        "progress_lavaarena_armor_hpextraheavy": "Велики истрајни оклоп",
+        "progress_lavaarena_armor_hpdamager": "Велики зупчасти оклоп",
+        "progress_lavaarena_armor_hprecharger": "Велики свилени оклоп",
+        "progress_lavaarena_armor_hppetmastery": "Велики шапутајући оклоп",
+        "progress_lavaarena_heavyblade": "Ковачева Оштрица",
+        "progress_lavaarena_firebomb": "Кристализована одаја магме",
+        "progress_rhinodrill": "Песницарог",
+        "progress_trails": "Вепарила",
+        "progress_lastsync": "Последњи пут ажурирано %1",
+        "progress_lastsync_title": "Последњи пут ажурирано %1",
+        "leaderboards-title": "Најбољи тимови",
+        "leaderboards-loading": "Учитавање најбољих тимова...",
+        "leaderboards-noresults": "Нема резултата.. Покушајте касније",
+        "leaderboards-pos": "%1",
+        "leaderboards-time": "Време",
+        "leaderboards-seconds": "с",
+        "leaderboards-deaths": "Број смрти",
+        "leaderboards-showmore": "Прикажи више",
+        "leaderboards-showless": "Прикажи мање",
+        "date": {
+            "parentLocale": 'en',
+            "months": 'Јануара_Фебруара_Марта_Априла_Маја_Јуна_Јула_Августа_Септембра_Октобра_Новембра_Децембра'.split('_'),
+            "longDateFormat": {
+				LLL: 'D[.]  MMMM  YYYY[. у]  HH:mm'
+				
+            },
+            "relativeTime": {
+                "future": "за %s",
+                "past": "пре %s",
+                "s": 'пар секунди',
+                "ss": '%d секунди',
+                "m": "једног минута",
+                "mm": "%d минута",
+                "h": "једног сата",
+                "hh": "%d сати",
+                "d": "једног дана",
+                "dd": "%d дана",
+                "M": "једног месеца",
+                "MM": "%d месеци",
+                "y": "једне године",
+                "yy": "%d година"
+            },
+            "ordinal": function(number) {
+                return number + ".";
+            }
+        },
+        "localization-credits": "Заслуге локализација",
+        "localization-help": "Да ли желиш да помогнеш?",
+        "localization-helptext": "Желиш ли додати свој језик? Поднеси захтев на %1."
+	},
